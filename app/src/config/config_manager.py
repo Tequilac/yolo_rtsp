865f79e444e3996e558f29c8a0bd04a02b64630a
Tequilac <58514571+tequilac@users.noreply.github.com>
2022-11-20 12:42:58 +0100

    Fixes

--- app/src/config/config_manager.py
+++ app/src/config/config_manager.py
@@ -71,12 +71,13 @@ class ConfigManager:
     def ditch_current(self):
         self._frames_manager = None
         self._rtsp_reader = None
+        self._has_config = False
 
     def reload_config(self, config: Config):
         if self._rtsp_reader:
             self._rtsp_reader.stop_capture()
         if self._frames_manager:
             self._frames_manager.stop()
-        print(config)
+        self._has_config = True
         self._frames_manager = FramesManager(config.frame_strategy, config.mqtt_info)
         self._rtsp_reader = RtspReader(config.frame_rate_timeout, config.rtsp_url, self._frames_manager)
