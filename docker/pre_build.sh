cf0b57d351955b134abfa2a8a51688c093c7e2f4
Tequilac <58514571+tequilac@users.noreply.github.com>
2022-11-26 15:55:01 +0100

    Docker

--- docker/pre_build.sh
+++ docker/pre_build.sh
@@ -1,5 +1,5 @@
 sudo apt install -y qemu-user-static binfmt-support
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --name sofia # name as you like
-docker buildx use sofia
+docker buildx create --name profile
+docker buildx use profile
 docker buildx inspect --bootstrap
